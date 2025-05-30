60f74e170b643d04af3af9eb91db0eb9da932236
57R0LL3R <drcecontre@gmail.com>
2025-05-29 20:55:13 -0500

    Agregar archivos de proyecto.

---
+++ savings_tool_dotnet(MVC)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace savings_tool_dotnet_MVC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
